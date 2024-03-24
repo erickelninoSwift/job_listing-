f5b7bff3df033e7c7eede4e4fc7ae645a4697e3e
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-24 05:41:08 +0200

    update data

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,13 @@ import Homecard from "./components/Homecard";
 import JobsListing from "./components/JobsListing";
 
 const App = () => {
-  const headerTitle = {
-    title: " Become a React Dev",
-    subtitle: "Find the React job that fits your skills and needs",
-  };
   return (
     <>
       <div>
         <Navbar />
-        <Hero title={headerTitle.title} subtitle={headerTitle.subtitle} />
+        <Hero />
         <Homecard />
         <JobsListing />
-        <section className="m-auto max-w-lg my-10 px-6">
-          <a
-            href="jobs.html"
-            className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >
-            View All Jobs
-          </a>
-        </section>
       </div>
     </>
   );
--- src/components/JobsListing.jsx
+++ src/components/JobsListing.jsx
@@ -5,19 +5,29 @@ import Job from "./Job";
 const JobsListing = () => {
   const recentJobs = Jobs.jobs.slice(0, 3);
   return (
-    <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((data, index) => {
-            const { id } = data;
-            return <Job key={id} positionDetails={data} />;
-          })}
+    <>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentJobs.map((data, index) => {
+              const { id } = data;
+              return <Job key={id} positionDetails={data} />;
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
   );
 };
 
