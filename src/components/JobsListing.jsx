d7ab67705359bec2844ab6a387107fdfa8aa5ccc
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-24 04:55:21 +0200

    update data

---
+++ src/components/JobsListing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Jobs from "../jobs.json";
+import Job from "./Job";
+
+const JobsListing = () => {
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {Jobs.jobs.map((data) => {
+            const { id } = data;
+            return <Job key={id} positionDetails={data} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobsListing;
