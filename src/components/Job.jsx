409169908a7db039910e74781326bc1cfda5cb2d
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-24 17:00:44 +0200

    add react router dom

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-
+import { FaMapMarker } from "react-icons/fa";
 const Job = ({ positionDetails }) => {
   const { id, type, title, description, salary, location } = positionDetails;
   const [showDescription, setshowDescription] = useState(false);
@@ -33,7 +33,7 @@ const Job = ({ positionDetails }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1" />
             {location}
           </div>
           <a
